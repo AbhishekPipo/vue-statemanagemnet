8f304f8a637126e15f3aff289fb2393ead209c1e
PWS <pws@PWSs-MacBook-Air.local>
2024-09-03 00:42:04 -0700

    first statemanagemenet app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script >
+import Counter from './components/counter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<template>
+<div class="">
+
+<Counter></Counter>
+
+
+{{$store.state.count}}
+</div>
+</template>
+
+<style >
+
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="">
+        {{ $store.state.count }}
+        </div>
+        <div class="">
+            <button @click.prevent="onIncreament()" style="margin-right: 10px;" >+</button>
+            <button @click.prevent="onDecreament()" >-</button>  
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return {
+          
+        }
+    },
+    methods:{
+        onIncreament(){
+            this.$store.state.count++;
+        },
+        onDecreament(){
+            this.$store.state.count--;
+        }
+    }
+}
+</script>
